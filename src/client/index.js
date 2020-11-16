d1627a9a3965411a1a84eaf9aa2c5e14b39441e4
Andrew Coulon <zlogik@outlook.com>
2020-11-16 16:52:02 -0500

    Client library

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,14 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+
 import './styles/base.scss'
 
 console.log(checkForName);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+export {
+    checkForName,
+    handleSubmit
+}
