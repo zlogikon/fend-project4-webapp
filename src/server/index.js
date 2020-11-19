96c9fd551b7f90ab0a66015e24d9f02f920eb3cd
Andrew Coulon <zlogik@outlook.com>
2020-11-19 16:46:17 -0500

    create server

--- src/server/index.js
+++ src/server/index.js
@@ -1,16 +1,29 @@
 const dotenv = require('dotenv');
 dotenv.config();
-
-var path = require('path')
 const apiKey = process.env.API_KEY
+
+
+const path = require('path')
 const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
+const cors = require('cors')
+const fetch = require('node-fetch')
+const bodyParser = require('body-parser')
 
 const app = express()
 
-app.use(express.static('dist'))
+const port = 8080
 
-console.log(__dirname)
+app.use(cors())
+app.use(express.static('dist'))
+app.use(bodyParser.text())
+
+URLBase = 'https://api.meaningcloud.com/sentiment-2.1?key='
+URLLang = '&lang=auto&url='
+TestURL = 'https://www.foxnews.com/politics/giuliani-presses-trump-election-challenge-case-in-fiery-news-conference'
+
+const resURL= URLBase + apiKey + URLLang +  TestURL
+
 
 app.get('/', function (req, res) {
     // res.sendFile('dist/index.html')
@@ -18,12 +31,10 @@ app.get('/', function (req, res) {
 })
 
 // designates what port the app will listen to for incoming requests
-app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+app.listen(port, function () {
+    console.log(`Example app listening on port ${port}!`)
 })
 
 app.get('/test', function (req, res) {
     res.send(mockAPIResponse)
 })
-
-console.log(`Your API key is ${apiKey}`);
