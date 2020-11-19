d7d74271202ffa532291345692ef2d7244fa56cd
Andrew Coulon <zlogik@outlook.com>
2020-11-19 16:46:58 -0500

    cleanup

--- src/client/js/URLChecker.js
+++ src/client/js/URLChecker.js
@@ -1,22 +1,9 @@
-function checkURL(userInput) {
+//I borrowed this regex code from https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-9.php
+//as advanced regex was not covered in the course
+
+export function checkURL(userInput) {
 
     let regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
         
-    if (regexp.test(userInput))
-        {
-            console.log('True')
-            return true;
-        }
-        else
-        {
-            console.log('False');
-            return false;
-        }
-       
+    return (regexp.test(userInput))   
 }
-
-console.log(checkURL("http://www.example.com"));
-console.log(checkURL("https://www.example.com"));
-console.log(checkURL("www.example.com"));
-
-export { checkURL }
