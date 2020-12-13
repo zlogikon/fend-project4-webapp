28e803a637f0c01322e456c7469216c2772d1c5f
Andrew <zlogik@outlook.com>
2020-12-13 16:05:56 -0500

    remove test url checker and create original test

--- src/client/js/URLChecker.js
+++ src/client/js/URLChecker.js
@@ -1,9 +1,9 @@
-//I borrowed this regex code from https://www.w3resource.com/javascript-exercises/javascript-regexp-exercise-9.php
-//as advanced regex was not covered in the course
-
 export function checkURL(userInput) {
 
-    let regexp =  /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    let http =  /^(?:http(s)?:\/\/)/;
+    let space = /\s/;
         
-    return (regexp.test(userInput))   
+    return (http.test(userInput) && !space.test(userInput))   
+
+    
 }
