d49bf3fd79209c8c1ff434c3249ea7e2badbd1e5
Andrew <zlogik@outlook.com>
2020-12-14 09:43:19 -0500

    formHandler testing

---
+++ __Test__/formHandler.test.js
@@ -0,0 +1,9 @@
+import { postData } from '../src/client/js/formHandler';
+
+describe("Testing the URLChecker functionality", () => {
+
+    test('this is postData test', async () => {
+        expect(postData).toBeDefined();
+      }); 
+
+})
